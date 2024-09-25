16c3fa27a4810ff2a783b3c5d6db4c5d0049096e
ArthurTheDog0101 <129325489+ArthurTheDog0101@users.noreply.github.com>
2024-09-25 09:32:08 -0300

    js

---
+++ main.js
@@ -0,0 +1,6 @@
+const url/="https://servicodados.ibge.gov.br/api/v3/agregados/1286/periodos/1920/variaveis/590?localidades=N3[27,28,29,43,50]"
+
+async function VisualizarDados() {
+    const res = await fetch(url)
+    const dados = await res.json()
+}
